dbdcc5e3737c4fdff69402161b73585a024b93dc
bizip <bizip04@gmail.com>
2022-05-19 08:38:06 +0200

    Add home image section

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <section>
             <div><SideBar /></div>
-            <div>
+            <div className="home__base">
                 <div className="home__header"><h1>Journalism. Storytelling. Brand Communication.</h1></div>
                 <div className="home__image"><img src="https://debola.s3.eu-west-2.amazonaws.com/Image004_8c2e2dcda8.jpg" alt="debola" /></div>
                 <div className="home__social"><h1>Home section</h1></div>
