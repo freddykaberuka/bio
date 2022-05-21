754f34ea5ff1beadd24f90f5f756fff6f20296f9
bizip <bizip04@gmail.com>
2022-05-21 07:50:07 +0200

    Add a home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,11 @@ const Home = () => {
             <div className="home__base">
                 <div className="home__header"><h1>Journalism. Storytelling. Brand Communication.</h1></div>
                 <div className="home__image"><img src="https://debola.s3.eu-west-2.amazonaws.com/Image004_8c2e2dcda8.jpg" alt="debola" /></div>
-                <div className="home__social"><h1>Home section</h1></div>
+                <div className="home__social">
+                    <ul>
+                        {/* <li><a><img src={ } alt="twitter" ></img> </a></li> */}
+                    </ul>
+                </div>
             </div>
         </section>
     )
