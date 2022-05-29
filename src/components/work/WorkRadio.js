a1a1909557c7b26aea6a7a840b265414335186b7
bizip <bizip04@gmail.com>
2022-05-29 23:03:54 +0200

    Add image gallery

--- src/components/work/WorkRadio.js
+++ src/components/work/WorkRadio.js
@@ -10,6 +10,37 @@ import SideBar from "../sideBar";
 import Popup from "../Popup";
 const WorkRadio = () => {
     const [onPopup, setOnPopup] = useState(false);
+    const workData = [{
+        workImage: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        workTitle: 'Senior legal adviser',
+        someDetails: 'Does your lorem ipsum text long for something a little meatier'
+    },
+    {
+        workImage: 'https://images.unsplash.com/photo-1559523182-a284c3fb7cff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+        workTitle: 'Architect & Engineer',
+        someDetails: 'Does your lorem ipsum text long for something a little meatier'
+    },
+    {
+        workImage: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        workTitle: 'Senior legal adviser',
+        someDetails: 'Does your lorem ipsum text long for something a little meatier'
+    },
+    {
+        workImage: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        workTitle: 'Senior legal adviser',
+        someDetails: 'Does your lorem ipsum text long for something a little meatier'
+    },
+    {
+        workImage: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        workTitle: 'Senior legal adviser',
+        someDetails: 'Does your lorem ipsum text long for something a little meatier'
+    },
+    {
+        workImage: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        workTitle: 'Senior legal adviser',
+        someDetails: 'Does your lorem ipsum text long for something a little meatier'
+    },
+    ];
 
     return (
         <section>
@@ -23,7 +54,21 @@ const WorkRadio = () => {
             <div className="home__base">
                 <div className="home__header"><h1>Assistant TV presenter</h1></div>
                 <div className="work__grid">
-                    
+
+
+                    {workData.map((item, index) =>
+                    (<div className="work__card" key={index} >
+                        <div class="card">
+                            <img src={item.workImage} alt="Avatar" />
+                            <div class="container">
+                                <h4><b>{item.workTitle}</b></h4>
+                                <p>{item.someDetails}</p>
+                            </div>
+                        </div>
+                    </div>)
+                    )}
+
+
 
                 </div>
                 {/* <div className="home__social">
