ae64be018659e1cf7e8e65728ef8928e8b9acee1
bizip <bizip04@gmail.com>
2022-05-30 18:37:49 +0200

    Add about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import WorkRadio from './components/work/WorkRadio';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/radio" element={<WorkRadio />} />
+        <Route path="/about" element={<AboutPage />} />
+
       </Routes>
     </Router>
   );
