4f790825a814eaef2af0ed6b329f7d63d69fa4f0
bizip <bizip04@gmail.com>
2022-06-03 19:02:08 +0200

    Add a gallery section

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import WorkNav from "./sidebar/workNav";
-import skillsNav from "./skills/skillsNava";
 import SkillsNav from "./skills/skillsNava";
 const SideBar = () => {
     const [work, setwork] = useState(false);
--- src/pages/Garelly.js
+++ src/pages/Garelly.js
@@ -5,7 +5,8 @@ import linkedin from "../img/linkedin.svg";
 import instagram from "../img/instagram.svg";
 import medium from '../img/medium.svg';
 import menu from '../img/menu.svg';
-import classes from './About.module.css'
+import style from './Gallery.module.css';
+
 
 import SideBar from "../components/sideBar";
 import Popup from "../components/Popup";
@@ -22,9 +23,72 @@ const Gallery = () => {
             </div>
 
             <div className="home__base">
-                <div className="home__header"><h1>About me</h1></div>
-                <div className={classes.about__section}>
-                    <h1>Gallery stuff goes here</h1>
+                <div className="home__header"><h1>Gallery</h1></div>
+                <div className={style.container}>
+
+                    <div className={style.wrap}>
+                        <div className={style.gallery}>
+                            <figure className={`${style.gallery__item} ${style['gallery__item--1']}`}>
+                                <a href="#img1">
+                                    <img src="https://images.unsplash.com/photo-1574270981993-f1df213562b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="01" className={style.gallery__img} />
+                                </a>
+                                <div className={style.lightbox} id="img1">
+                                    <img src="https://images.unsplash.com/photo-1574270981993-f1df213562b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="item" />
+                                    <a href="#_" className={style['btn-close']}>&times;</a>
+                                </div>
+                            </figure>
+
+                            <figure className={`${style.gallery__item} ${style['gallery__item--2']}`}>
+                                <a href="#img2">
+                                    <img src="https://images.unsplash.com/photo-1573743338941-39db12ef9b64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="02" className={style.gallery__img} />
+                                </a>
+                                <div className={style.lightbox} id="img2">
+                                    <img src="https://images.unsplash.com/photo-1573743338941-39db12ef9b64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="item" />
+                                    <a href="#_" className={style['btn-close']}>&times;</a>
+                                </div>
+                            </figure>
+
+                            <figure className={`${style.gallery__item} ${style['gallery__item--3']}`}>
+                                <a href="#img3">
+                                    <img src="https://images.unsplash.com/photo-1572295727871-7638149ea3d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="03" className={style.gallery__img} />
+                                </a>
+                                <div className={style.lightbox} id="img3">
+                                    <img src="https://images.unsplash.com/photo-1572295727871-7638149ea3d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="item" />
+                                    <a href="#_" className={style['btn-close']}>&times;</a>
+                                </div>
+                            </figure>
+
+                            <figure className={`${style.gallery__item} ${style['gallery__item--4']}`}>
+                                <a href="#img4">
+                                    <img src="https://images.unsplash.com/photo-1571680719972-f18bb57077cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="04" className={style.gallery__img} />
+                                </a>
+                                <div className={style.lightbox} id="img4">
+                                    <img src="https://images.unsplash.com/photo-1571680719972-f18bb57077cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="item" />
+                                    <a href="#_" className={style['btn-close']}>&times;</a>
+                                </div>
+                            </figure>
+
+                            <figure className={`${style.gallery__item} ${style['gallery__item--5']}`}>
+                                <a href="#img5">
+                                    <img src="https://images.unsplash.com/photo-1571586100127-cdaef780fc61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="05" className={style.gallery__img} />
+                                </a>
+                                <div className={style.lightbox} id="img5">
+                                    <img src="https://images.unsplash.com/photo-1571586100127-cdaef780fc61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="item" />
+                                    <a href="#_" className={style['btn-close']}>&times;  </a>
+                                </div>
+                            </figure>
+
+                            <figure className={`${style.gallery__item} ${style['gallery__item--6']}`}>
+                                <a href="#img6">
+                                    <img src="https://images.unsplash.com/photo-1568473648251-3a0c3aa56192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="06" className={style.gallery__img} />
+                                </a>
+                                <div className={style.lightbox} id="img6">
+                                    <img src="https://images.unsplash.com/photo-1568473648251-3a0c3aa56192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="item" />
+                                    <a href="#hhh" className={style['btn-close']}>&times;</a>
+                                </div>
+                            </figure>
+                        </div>
+                    </div>
                 </div>
                 <div className="work__footer">
                     <div className="home__social">
