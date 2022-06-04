665394714384d9cde96e48c4babd140b3f91acaa
bizip <bizip04@gmail.com>
2022-06-04 19:24:21 +0200

    Add home animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,13 @@ import linkedin from "../img/linkedin.svg";
 import instagram from "../img/instagram.svg";
 import medium from '../img/medium.svg';
 import menu from '../img/menu.svg';
+import classes from './Home.module.css';
 
 import SideBar from "./sideBar";
 import Popup from "./Popup";
+import HomeCarousel from "./HomeCarousel";
 const Home = () => {
     const [onPopup, setOnPopup] = useState(false);
-
     return (
         <section>
             <div className="none"> <Popup triger={onPopup} setTrigger={setOnPopup} /> </div>
@@ -22,18 +23,22 @@ const Home = () => {
 
             <div className="home__base">
                 <div className="home__header"><h1>Journalism. Storytelling. Brand Communication.</h1></div>
-                <div className="home__image"><img src="https://debola.s3.eu-west-2.amazonaws.com/Image004_8c2e2dcda8.jpg" alt="debola" /></div>
-                <div className="home__social">
-                    <ul>
-                        <li><a href="#titer"><img src={linkedin} alt="Linkedin" ></img></a></li>
-                        <li><a href="#titer"><img src={twitter} alt="twitter" ></img></a></li>
-                        <li><a href="#titer"><img src={facebook} alt="facebook" ></img></a></li>
-                        <li><a href="#titer"><img src={instagram} alt="instagram" ></img></a></li>
-                        <li><a href="#titer"><img src={medium} alt="instagram" ></img></a></li>
-                    </ul>
-                </div>
+                <div className="home__image">
+                    <div className={classes.container}>
+                        <HomeCarousel />
+                    </div>
+                    <div className="home__social">
+                        <ul>
+                            <li><a href="#titer"><img src={linkedin} alt="Linkedin" ></img></a></li>
+                            <li><a href="#titer"><img src={twitter} alt="twitter" ></img></a></li>
+                            <li><a href="#titer"><img src={facebook} alt="facebook" ></img></a></li>
+                            <li><a href="#titer"><img src={instagram} alt="instagram" ></img></a></li>
+                            <li><a href="#titer"><img src={medium} alt="instagram" ></img></a></li>
+                        </ul>
+                    </div>
+                </div >
             </div>
-        </section>
+        </section >
     )
 
 }
