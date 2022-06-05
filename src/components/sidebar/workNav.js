ab7ec01ef61bdaa5bb2b6105661cb12e4056aff5
bizip <bizip04@gmail.com>
2022-06-05 08:59:08 +0200

    Fix work broken link

--- src/components/sidebar/workNav.js
+++ src/components/sidebar/workNav.js
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom';
 const WorkNav = (props) => {
     return (props.isWork === true) ? (<ul>
         <li><NavLink to='/Radio'>Radio</NavLink></li>
-        <li><NavLink to='/arena'>Kigali arena</NavLink></li>
     </ul>) : '';
 }
 export default WorkNav;
