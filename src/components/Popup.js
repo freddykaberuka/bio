ae60bead880e9c32e96158cf0b1500ad170c5179
bizip <bizip04@gmail.com>
2022-06-06 08:14:18 +0200

    Fixed mobile links

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,19 +1,39 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import close from '../img/close.svg';
+import WorkNav from './sidebar/workNav';
+import SkillsNav from '../components/skills/skillsNava';
+
 const Popup = (props) => {
+    const [work, setwork] = useState(false);
+    const [skillSection, setskills] = useState(false);
+
+    const toggleWork = () => {
+        setwork(prev => !prev)
+    }
+    const toggleSills = () => {
+        setskills(prev => !prev)
+    }
+
     return (props.triger) ? (<div className='mobile__popup' >
         <div className="mobile__close">
             <h1>Debola <span>Adebanjo</span></h1>
             <img src={close} onClick={() => props.setTrigger(false)} alt="close icon" />
         </div>
         <ul>
-            <li><a href="#g">Home</a></li>
-            <li><a href="#g">Works</a></li>
-            <li><a href="#g">Projects & collabolations</a></li>
-            <li><a href="#g">Skills & recommendations</a></li>
-            <li><a href="#g">Press and awards</a></li>
-            <li><a href="#g">About me</a></li>
-            <li><a href="#g">Gallery</a></li>
-            <li><a href="#g">Let's work</a></li>
+            <li><NavLink  className='navlink' to='/' >Home</NavLink></li>
+            <li onClick={toggleWork} className="work_li">Works</li>
+            <WorkNav  className='navlink' isWork={work} />
+            <li></li>
+            <li><NavLink className='navlink'  to='/project'>Projects & collabolations</NavLink></li>
+            <li onClick={toggleSills} className="work_li">Skills & recommendations</li>
+            <SkillsNav className='navlink'  isSkills={skillSection} />
+            <li></li>
+
+            <li><NavLink className='navlink'  to='/award'>Press and awards</NavLink></li>
+            <li><NavLink className='navlink'  to='/about'>About me</NavLink></li>
+            <li><NavLink className='navlink'  to='/Gallery'>Gallery</NavLink></li>
+            <li><NavLink className='navlink'  to='/contact'>Let's work</NavLink></li>
         </ul>
     </div>) : "";
 }
